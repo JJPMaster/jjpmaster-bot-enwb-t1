ca8796ba381d94819959a3f38807e200419ee376
Juniper <78509994+JJPMaster@users.noreply.github.com>
2024-12-12 18:08:28 -0500

    replace with enwikibooks version

--- UpdateGroupJson.py
+++ UpdateGroupJson.py
@@ -1,5 +1,5 @@
 # ------------------------------------------------------------------------------
-#  Copyright (C) 2020 Michael Daniels
+#  Copyright (C) 2024 Juniper J. Ponce
 #
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -19,13 +19,12 @@ import pywikibot
 import json
 from datetime import datetime, timezone
 
-site = pywikibot.Site('en', 'wikipedia')
+site = pywikibot.Site('en', 'wikibooks')
 metawiki = pywikibot.Site('meta', 'meta')
 
 
 def globalallusers(site, group):
     """
-
     :param site: The site to get our data from.
     :type site: pywikibot.site.APISite
     :param group: The group to get.
@@ -51,43 +50,34 @@ def sortkeys(key: str) -> str:
     """
     sortkeyDict = {
         "sysop": "A",
-        "arbcom": "ARB",
         "bureaucrat": "B",
         "checkuser": "CU",
-        "oversight": "CV",
+        "global-sysop": "GS",
+        "suppress": "OS",
         "interface-admin": "IA",
-        "abusefilter": "EFM",
-        "abusefilter-helper": "EFH",
         "accountcreator": "ACC",
-        "autoreviewer": "AP",
-        "extendedmover": "PM",
-        "filemover": "FM",
-        "massmessage-sender": "MMS",
-        "patroller": "NPR",
-        "reviewer": "PCR",
-        "rollbacker": "RB",
+        "autoreview": "AR",
+        "editor": "REV",
         "global-renamer": "GRe",
         "global-rollbacker": "GRb",
-        "templateeditor": "TE",
-        "otrs-member": "OTRS",
+        "vrt-permissions": "VRT",
+        "ombuds": "Omb",
         "steward": "S"
     }
     return sortkeyDict[key]
 
 
-combinedJsDataPage = pywikibot.Page(site, "User:MDanielsBot/markAdmins-Data.js")
-combinedJsonDataPage = pywikibot.Page(site, "User:MDanielsBot/markAdmins-Data.json")
+combinedJsDataPage = pywikibot.Page(site, "User:JJPMaster (bot)/markAdmins-Data.js")
+combinedJsonDataPage = pywikibot.Page(site, "User:JJPMaster (bot)/markAdmins-Data.json")
 
 
-localGroups = ["abusefilter", "abusefilter-helper", "accountcreator",
-               "bureaucrat", "checkuser", "extendedmover", "filemover",
-               "interface-admin", "massmessage-sender", "oversight",
-               "sysop", "templateeditor"]
-extraLocalGroups = ["autoreviewer", "patroller", "reviewer", "rollbacker"]
-globalGroups = ["otrs-member", "steward", "global-rollbacker"]
+localGroups = ["accountcreator",
+               "bureaucrat", "checkuser",
+               "interface-admin", "suppress",
+               "sysop"]
+extraLocalGroups = ["autoreview", "editor"]
+globalGroups = ["vrt-permissions", "steward", "global-rollbacker", "global-sysop", "ombuds"]
 metaGroups = ["global-renamer"]
-arbcomJson = pywikibot.Page(site, "User:AmoryBot/crathighlighter.js/arbcom.json").get()
-arbcom_members = json.loads(arbcomJson)
 
 outputDict = {}
 
@@ -107,12 +97,6 @@ for group in globalGroups:
             outputDict[user['name']].append(group)
         else:
             outputDict[user['name']] = [group]
-
-for user in arbcom_members:
-    if user in outputDict.keys():
-        outputDict[user].append("arbcom")
-    else:
-        outputDict[user] = ["arbcom"]
 
 for group in extraLocalGroups:
     for user in site.allusers(group=group):
@@ -148,8 +132,8 @@ oldJsonpage = combinedJsonDataPage.get()
 if newText != oldJspage or outputJson != oldJsonpage:
     print(datetime.now(timezone.utc).strftime("%b %d %Y %H:%M:%S.%f")
           + " -- Updated!", flush=True)
-    combinedJsDataPage.put(newText, "Update markadmins data")
-    combinedJsonDataPage.put(outputJson, "Update markadmins data")
+    combinedJsDataPage.put(newText, "Update markAdmins data")
+    combinedJsonDataPage.put(outputJson, "Update markAdmins data")
 else:
     print(datetime.now(timezone.utc).strftime("%b %d %Y %H:%M:%S.%f")
           + " -- No changes", flush=True)
